79cc86b1c7fafd0c21ddcb727594c1160bc6621b
Igor Cardoso <igorgcardoso7@gmail.com>
2022-12-19 09:22:58 -0300

    Pós ignite

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: [],
+      },
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
